d00c45489b757c2ad2bc7956b1ddddf106170b96
Zoltage <jacob.mathewsaz@gmail.com>
2021-09-09 14:29:25 -0700

    update1

--- Gui.py
+++ Gui.py
@@ -1,26 +1,19 @@
-# Jacob Mathews
-# TicTacToe
-# 9 / 9 / 21
-
 from tkinter import *
 import sys
 
 win = Tk()
-win.option_add("*font", "roboto 20")
-win.configure(bg="#000000")
 
+class GuiController():
+    def __init__(self):
+        self.buttoncolor = "#F2002C"
+        self.bgcolor = "#0c0c0c"
 
-def Main():
-    Clear()
+    def Main(self):
+        Clear()
 
-    Label(win, text= "DGS LLC Database", fg='#64ffff', bg='#646464').grid(row=1, column=1)
-    Label(win, text='', bg='#000000').grid(row=2, column=0)
+        Label(win, text='Tic Tac Toe', bg=self.buttoncolor )
 
-    Button(win, text="Reports", command=reportMenu, padx=16, bd=2, relief=SOLID).grid(row=3, column=0,sticky='nesw')
-    Button(win, text="Exit", command=endWin, bg="#646464", fg="#64ffff", bd=2, relief=SOLID).grid(row=3, column=2,sticky='nesw')
-    Button(win, text="Enter / Edit\nCustomers", command=customerMenu, bd=2, relief=SOLID).grid(row=4, column=0)
-    Button(win, text="Enter / Edit\nProducts", command=productMenu, bd=2, relief=SOLID).grid(row=4, column=1)
-    Button(win, text="Enter / Edit\nSales", command=salesMenu, bd=2, relief=SOLID).grid(row=4, column=2)
+    win.mainloop()
 
 def Clear():
     for widget in win.winfo_children():
@@ -30,4 +23,4 @@ def Exit():
     sys.exit()
 
 Main()
-win.mainloop()
+
---
+++ Main.py
@@ -0,0 +1,11 @@
+# Jacob Mathews
+# TicTacToe
+# 9 / 9 / 21
+
+from tkinter import *
+import Gui
+
+if __name__ =="__main__":
+    Gui.GuiController()
+
+    Gui.win.option_add("*font", "roboto 20")
